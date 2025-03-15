39a48c069aaf4c67fe72f366b4353989be594e60
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-03-15 22:51:58 +0100

    update init file

--- src/elicito/__init__.py
+++ src/elicito/__init__.py
@@ -4,4 +4,28 @@ A Python package for learning prior distributions based on expert knowledge
 
 import importlib.metadata
 
+from elicito import (
+    initialization,
+    losses,
+    networks,
+    optimization,
+    plots,
+    simulations,
+    targets,
+    types,
+    utils,
+)
+
 __version__ = importlib.metadata.version("elicito")
+
+__all__ = [
+    "initialization",
+    "losses",
+    "networks",
+    "optimization",
+    "plots",
+    "simulations",
+    "targets",
+    "types",
+    "utils",
+]
