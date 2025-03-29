f8523cb296e57773ee77e6f45bb76703a9203a87
Florence Bockting <48919471+florence-bockting@users.noreply.github.com>
2025-03-29 09:28:49 +0100

    fix mypy and pandas import

--- src/elicito/initialization.py
+++ src/elicito/initialization.py
@@ -84,7 +84,7 @@ def uniform_samples(  # noqa: PLR0913, PLR0912, PLR0915
 
     """
     try:
-        from scipy.stats import qmc
+        from scipy.stats import qmc  # type: ignore [import-untyped]
     except ImportError as exc:
         raise MissingOptionalDependencyError("scipy", requirement="scipy") from exc
 
