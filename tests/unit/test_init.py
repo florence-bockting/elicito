3788be6b0ec9f1e84873571ff9d27453a0332b6e
florence-bockting <florence.bockting@tu-dortmund.de>
2025-10-21 15:12:07 +0200

    add tests for checks in Elicit object

--- tests/unit/test_init.py
+++ tests/unit/test_init.py
@@ -25,10 +25,13 @@ def eliobj():
         model=el.model(TestModel),
         parameters=[
             el.parameter(
-                name="b0",
+                name=f"b{i}",
                 family=tfd.Normal,
-                hyperparams=dict(loc=el.hyper(name="mu0"), scale=el.hyper("sigma0")),
+                hyperparams=dict(
+                    loc=el.hyper(name=f"mu{i}"), scale=el.hyper(f"sigma{i}")
+                ),
             )
+            for i in range(2)
         ],
         targets=[
             el.target(name="b0", loss=el.losses.L2, query=el.queries.quantiles((0.5,)))
@@ -41,6 +44,30 @@ def eliobj():
             distribution=el.initialization.uniform(radius=1, mean=0),
             iterations=1,
         ),
+        network=None,
+    )
+
+
+@pytest.fixture
+def network():
+    return el.networks.NF(
+        inference_network=el.networks.InvertibleNetwork,
+        network_specs=dict(
+            num_params=2,
+            num_coupling_layers=3,
+            coupling_design="affine",
+            coupling_settings={
+                "dropout": False,
+                "dense_args": {
+                    "units": 128,
+                    "activation": "relu",
+                    "kernel_regularizer": None,
+                },
+                "num_dense": 2,
+            },
+            permutation="fixed",
+        ),
+        base_distribution=el.networks.base_normal,
     )
 
 
@@ -52,11 +79,8 @@ def test_expert_input(eliobj):
     assert dat_format["data"].keys() == expected_format.keys()
 
     # test wrong format
-    eliobj.update(expert=el.expert.data({"b0": 0.5}))
-    dat_format_wrong = eliobj.expert
-
     with pytest.raises(AssertionError):
-        assert dat_format_wrong["data"].keys() == expected_format.keys()
+        eliobj.update(expert=el.expert.data({"b0": 0.5}))
 
     # test ground truth
     eliobj.update(expert=el.expert.simulator(ground_truth={"b0": tfd.Normal(0, 1)}))
@@ -64,14 +88,89 @@ def test_expert_input(eliobj):
     assert "ground_truth" in eliobj.expert.keys()
 
 
-def test_initializer_network(eliobj):
+def test_initializer_network(eliobj, network):
     # parametric method
     # correct: initialized is configured
     assert eliobj.initializer is not None
     assert eliobj.network is None
 
     # error message if initializer is None
-    eliobj.update(initializer=None, network="some_network")
-    ### TODO when updating the checks do not run
     with pytest.raises(ValueError):
-        assert eliobj.initializer is not None
+        eliobj.update(initializer=None, network=network)
+
+    # deep_prior method
+    # error message if network is None
+    with pytest.raises(ValueError):
+        eliobj.update(trainer=el.trainer(method="deep_prior", epochs=1, seed=123))
+
+    # correct
+    eliobj.update(
+        trainer=el.trainer(method="deep_prior", epochs=1, seed=123),
+        network=network,
+        initializer=None,
+    )
+
+    assert eliobj.network == network
+
+
+def test_shared_hyperparameter(eliobj):
+    # raise error if same name for hyperparameter
+    # but shared is False (default)
+    with pytest.raises(ValueError):
+        eliobj.update(
+            parameters=[
+                el.parameter(
+                    name=f"b{i}",
+                    family=tfd.Normal,
+                    hyperparams=dict(
+                        loc=el.hyper(name="mu0"), scale=el.hyper("sigma0")
+                    ),
+                )
+                for i in range(2)
+            ]
+        )
+
+    # correct; specify shared=True
+    eliobj.update(
+        parameters=[
+            el.parameter(
+                name=f"b{i}",
+                family=tfd.Normal,
+                hyperparams=dict(
+                    loc=el.hyper(name="mu0", shared=True),
+                    scale=el.hyper("sigma0", shared=True),
+                ),
+            )
+            for i in range(2)
+        ]
+    )
+
+    for i in range(2):
+        assert eliobj.parameters[i].hyperparams["loc"]["name"] == "mu0"
+        assert eliobj.parameters[i].hyperparams["scale"]["name"] == "sigma0"
+
+
+def test_initializer_setup(eliobj):
+    # correct; hyperparameter names match
+    eliobj.update(
+        initializer=el.initializer(
+            hyperparams=dict(mu0=0.1, sigma0=0.2, mu1=0.1, sigma1=0.2)
+        )
+    )
+
+    assert eliobj.initializer["hyperparams"]["mu0"] == 0.1
+    assert eliobj.initializer["hyperparams"]["sigma1"] == 0.2
+
+    # error; hyperparameter names do not match
+    with pytest.raises(ValueError):
+        eliobj.update(
+            initializer=el.initializer(
+                hyperparams=dict(mu2=0.1, sigma2=0.2, mu1=0.1, sigma1=0.2)
+            )
+        )
+
+
+def test_hyperparameters(eliobj):
+    # error; as parametric prior needs family and hyperparams arg.
+    with pytest.raises(ValueError):
+        eliobj.update(parameters=[el.parameter(name=f"b{i}") for i in range(2)])
