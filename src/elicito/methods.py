c11f11d098b40191c98ed70813d9a1ab7ca37161
Florence Bockting <florence.bockting@aalto.fi>
2026-09-04 11:28:17 +0300

    Move the input checks into the strategy objects

--- src/elicito/methods.py
+++ src/elicito/methods.py
@@ -6,7 +6,8 @@ from typing import Any, Protocol
 
 import tensorflow as tf
 
-from elicito.types import NFDict, Parameter, PriorMethods
+from elicito import networks
+from elicito.types import Initializer, NFDict, Parameter, PriorMethods
 
 
 def _constraints(parameters: list[Parameter]) -> dict[str, Any]:
@@ -75,6 +76,15 @@ class PriorMethod(Protocol):
         trainable_vars: Any,
     ) -> None:
         """Add the method-specific entries to the results."""
+        ...
+
+    def check(
+        self,
+        parameters: list[Parameter],
+        network: NFDict | None,
+        initializer: Initializer | None,
+    ) -> None:
+        """Raise if the sections are not valid for this method."""
         ...
 
 
@@ -209,6 +219,85 @@ class ParametricPrior:
         """Add the method-specific entries to the results."""
         res_ep["hyperparameter_gradient"] = gradients_ep
 
+    def check(
+        self,
+        parameters: list[Parameter],
+        network: NFDict | None,
+        initializer: Initializer | None,
+    ) -> None:
+        """Raise if the sections are not valid for this method."""
+        if initializer is None:
+            msg = (
+                "If method is 'parametric_prior', "
+                " the section 'initializer' can't be None."
+            )
+            raise ValueError(msg)
+
+        if network is not None:
+            msg = (
+                "If method is 'parametric prior' "
+                "the 'network' is not used and should be set to None."
+            )
+            raise ValueError(msg)
+
+        # check that hyperparameter names are not redundant
+        hyp_names = []
+        hyp_shared = []
+        for i in range(len(parameters)):
+            if parameters[i]["hyperparams"] is None:
+                msg = (
+                    "When using method='parametric_prior', the argument "
+                    "'hyperparams' of el.parameter "
+                    "cannot be None."
+                )
+                raise ValueError(msg)
+
+            hyp_names.append(
+                [
+                    parameters[i]["hyperparams"][key]["name"]
+                    for key in parameters[i]["hyperparams"].keys()
+                ]
+            )
+            hyp_shared.append(
+                [
+                    parameters[i]["hyperparams"][key]["shared"]
+                    for key in parameters[i]["hyperparams"].keys()
+                ]
+            )
+        # flatten nested list
+        hyp_names_flat = sum(hyp_names, [])  # noqa: RUF017
+        hyp_shared_flat = sum(hyp_shared, [])  # noqa: RUF017
+
+        hyperparams = initializer["hyperparams"]
+        if initializer["method"] is None and hyperparams is not None:
+            for k in hyperparams:
+                if k not in hyp_names_flat:
+                    msg = (
+                        f"Hyperparameter name '{k}' doesn't "
+                        "match any name specified in the parameters "
+                        "section. Have you misspelled the name?"
+                    )
+                    raise ValueError(msg)
+
+        seen = []
+        duplicate = []
+        share = []
+        for n, s in zip(hyp_names_flat, hyp_shared_flat):
+            if n not in seen:
+                seen.append(n)
+            elif s:
+                share.append(n)
+            else:
+                duplicate.append(n)
+
+        if len(duplicate) != 0:
+            msg = (
+                "The following hyperparameter have the same "
+                f"name but are not shared: {duplicate}. \n"
+                "Have you forgot to set shared=True?"
+            )
+            raise ValueError(msg)
+
 
 class DeepPrior:
     """Joint non-parametric prior via a normalizing flow."""
@@ -293,6 +382,42 @@ class DeepPrior:
     ) -> None:
         """Add the method-specific entries to the results."""
         output_res["num_NN_weights"] = [v.shape for v in trainable_vars]
+
+    def check(
+        self,
+        parameters: list[Parameter],
+        network: NFDict | None,
+        initializer: Initializer | None,
+    ) -> None:
+        """Raise if the sections are not valid for this method."""
+        if network is None:
+            msg = "If method is 'deep prior',  the section 'network' can't be None."
+            raise ValueError(msg)
+
+        if initializer is not None:
+            msg = (
+                "For method 'deep_prior' the "
+                "'initializer' is not used and should be set to None."
+            )
+            raise ValueError(msg)
+
+        if network["network_specs"]["num_params"] != len(parameters):
+            msg = (
+                "The number of model parameters as "
+                "specified in the parameters section, must match the "
+                "number of parameters specified in the network."
+                f"Expected {len(parameters)} but got "
+                f"{network['network_specs']['num_params']}"
+            )
+            raise ValueError(msg)
+
+        if network["base_distribution"].__class__ != networks.BaseNormal:
+            msg = (
+                "Currently only the standard normal distribution "
+                "is implemented as base distribution. "
+                "See GitHub issue #35."
+            )
+            raise NotImplementedError(msg)
 
 
 _METHODS: dict[str, PriorMethod] = {
