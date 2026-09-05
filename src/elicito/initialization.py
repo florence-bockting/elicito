ff18e787a0f79e271455e4b2375911cf62f1eb6e
Florence Bockting <florence.bockting@aalto.fi>
2026-09-05 17:11:39 +0300

    Import scipy only for the sobol and lhs methods

--- src/elicito/initialization.py
+++ src/elicito/initialization.py
@@ -83,11 +83,6 @@ def uniform_samples(  # noqa: PLR0913, PLR0912, PLR0915
         samples from the uniform distribution.
 
     """
-    try:
-        from scipy.stats import qmc
-    except ImportError as exc:
-        raise MissingOptionalDependencyError("scipy", requirement="scipy") from exc
-
     # set seed
     tf.random.set_seed(seed)
 
@@ -104,6 +99,14 @@ def uniform_samples(  # noqa: PLR0913, PLR0912, PLR0915
     if method not in ["sobol", "lhs", "random"]:
         msg = "Unsupported method. Choose from 'sobol', 'lhs', or 'random'."
         raise ValueError(msg)
+
+    if method in ("sobol", "lhs"):
+        try:
+            from scipy.stats import qmc
+        except ImportError as exc:
+            raise MissingOptionalDependencyError(
+                "scipy", requirement="scipy"
+            ) from exc
 
     # counter number of hyperparameters
     n_hypparam = 0
